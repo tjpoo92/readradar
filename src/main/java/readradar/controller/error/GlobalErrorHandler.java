2a8b3815640cd1817dff8808cc87215a6f5d1d25
Tyler Pooschke <tyler.pooschke@pm.me>
2023-12-14 23:35:00 -0600

    added globalerrorhandler for no such element

---
+++ src/main/java/readradar/controller/error/GlobalErrorHandler.java
@@ -0,0 +1,27 @@
+package readradar.controller.error;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalErrorHandler{
+
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(code = HttpStatus.NOT_FOUND)
+    public Map<String, String> handeNoSuchElementException(NoSuchElementException e){
+        log.info(e.toString());
+        Map<String, String> exceptionMap = new HashMap<>();
+        exceptionMap.put("message", e.toString());
+        return  exceptionMap;
+    }
+}
